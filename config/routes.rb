1e7dd3ec8772046eac9de2a9918910f5214a56ea
Sree <sboralingiah@icloud.com>
2016-11-22 18:08:39 +0000

    Adding links to home and about and changed root of site

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get "welcome/home" => "welcome#home"
+  get root :to => "welcome#home"
   
   get "welcome/about" => "welcome#about"
   
