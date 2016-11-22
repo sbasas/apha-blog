e6632dc26abc64641a14186ea9371e464cb232ab
Sree <sboralingiah@icloud.com>
2016-11-22 18:25:32 +0000

    Fix root page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root  "welcome#home"
   
-  get "welcome/about" => "welcome#about"
+  get "about" => "welcome#about"
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
