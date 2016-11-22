8a5ee6572ae8792540d2a69ece247f9c81be8462
Sree <sboralingiah@icloud.com>
2016-11-22 17:00:18 +0000

    Added home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get "welcome/home" => "welcome#home"
+  
+  get "welcome/about" => "welcome#about"
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
